421c772a1902957a33e7132b0d65ac13eaa9041d
melsowole <82158719+melsowole@users.noreply.github.com>
2021-09-09 22:07:54 +0200

    commits

--- Öv 2/Övningar/Ö1/index.js
+++ Öv 2/Övningar/Ö1/index.js
@@ -4,6 +4,15 @@ const buttons = [
     {
         name: "POST",
         svg: `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>`,
+        method: function (){
+            if( fName.value == "" || lName.value == "" || born.value =="" ){
+                PRINT("Wrong format", "error")
+            } else{
+                let obj = makeObj( getInputValue(fNAme), getInputValue(lName), getInputValue(born), "-")
+                console.log("obj")
+                // CALLDB()
+            }
+        }
     },
     {
         name: "DELETE",
@@ -16,6 +25,10 @@ const buttons = [
     {
         name:"GET",
         svg: `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>`
+    },
+    {
+        name:"CLEAR",
+        svg:`Clear all`,
     }
 ];
 const icons = document.getElementById("icons");
@@ -23,6 +36,7 @@ const msgBox = document.getElementById("msg-box");
 const fName = document.getElementById("first-name");
 const lName = document.getElementById("last-name");
 const born = document.getElementById("born");
+const idInput = document.getElementById("id")
 
 let person = {
     firstName: "Paul",
@@ -34,31 +48,38 @@ let edit = {id:98, firstName:"Anna"}
 
 let delID = {id:98}
 
+clearInputs()
+
 
 buttons.forEach(icon =>{
+
     let i = document.createElement("i");
     i.innerHTML = icon.svg;
     i.classList.add("icon");
 
-    i.addEventListener("click", ()=>{
-        let obj = {};
+    if( icon.name =="CLEAR"){
+        i.classList = "clear";
 
-        if( fName.value !== "" ){
-            obj.firstName = fName.value
-        }
+        i.addEventListener("click", () =>{
+            if( fName.value + lName.value + born.value + idInput.value == "") {
+                PRINT( "Fields are already cleared", "error" );
+                
+            } else {
+                clearInputs();
+                PRINT("All fields cleared", "success");
+            }
 
-        if( lName.value !== "" ){
-            obj.lastName = lName.value
-        }
+        })
+    } else{
 
-        if( born.value !== "" ){
-            obj.born = born.value;
-        }
-
+        i.addEventListener("click", ()=>{
         
 
-        CALLDB(icon.name, )
-    })
+            CALLDB(icon.name, )
+        })
+    }
+
+    
 
     icons.append(i)
 })
@@ -112,18 +133,13 @@ function CALLDB(action, obj){
     }) )
         .then( r => {
             if( r.status == 404 ){
-                let msg = document.createElement("p");
-                msg.textContent = "! This user does not exist in the database";
-                msg.classList.add("error")
-                msgBox.append(msg)
-                
+                PRINT("! This user does not exist in the database", "error")           
                 throw Error("User not in DB")
+
             } else if( r.status == 409 ){
-                let msg = document.createElement("p");
-                msg.textContent = "! ! A user already exists with the same first and last name";
-                msg.classList.add("error")
-                msgBox.append(msg)
+                PRINT("! ! A user already exists with the same first and last name", "error")
                 throw Error("User already in DB")
+
             } else{
                 r.json() 
             }
@@ -140,16 +156,35 @@ function CALLDB(action, obj){
         .catch( console.log("something went wrong") )
 }
 
-// GET()
+function makeObj(f, l, b, i){
+    let obj = {};
 
-// CALLDB("DELETE", delID)
+    if( f !== "-" )obj.firstName = f;
+    if( l !== "-" ) obj.lastName = l;
+    if ( b !== "-" ) obj.born = b;
+    if( i !== "-" ) obj.id = i;
+
+    return obj;
+}
 
 function getInputValue(input){
     return input.value[0].toUppercase() + input.value.slice(1).toLowercase();
 }
 
+function PRINT(m, c){
+    console.log(m)
+    let msg = document.createElement("p");
+    msg.textContent = m;
+    msg.classList.add(c);
+    msgBox.append(msg)
+}
 
-
+function clearInputs(){
+    fName.value="";
+    lName.value="";
+    born.value="";
+    idInput.value="";
+}
 
 
 
